283e409e9d7771a235a653f912fc47a0df08aaaf
caoqiang <caoqianghappy@126.com>
2023-12-17 15:16:09 +0800

    up

---
+++ javascript/extension_help.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function update_extensions(path) {
+    let textarea = gradioApp().querySelector('#extension_path textarea')
+    textarea.value = path
+    updateInput(textarea)
+    textarea.click()
+    gradioApp().querySelector('#update_extensions_btn').click()
+}
+
+function delete_extensions(path) {
+    if (confirm('确定要删除这个扩展吗？')) {
+        let textarea = gradioApp().querySelector('#template_id textarea')
+        textarea.value = path
+        updateInput(textarea)
+        textarea.click()
+        gradioApp().querySelector('#delete_extensions_btn').click()
+    }
+}
